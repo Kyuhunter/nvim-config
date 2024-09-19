ce52a4536772fc1e18c15b1f572e36fc7b89e0cc
Adrian Olschimke <adrian.olschimke+github@gmail.com>
2024-09-19 11:58:34 +0200

    put debugging plugins in their own file

---
+++ lua/plugins/debugging.lua
@@ -0,0 +1,29 @@
+return {
+    {
+        "rcarriga/nvim-dap-ui",
+        dependencies = {"mfussenegger/nvim-dap", "nvim-neotest/nvim-nio"}
+    },
+    {
+        "folke/lazydev.nvim",
+        ft = "lua", -- only load on lua files
+        opts = {
+            library = {
+                -- See the configuration section for more details
+                -- Load luvit types when the `vim.uv` word is found
+                { path = "luvit-meta/library", words = { "vim%.uv" } },
+                { plugins = { "nvim-dap-ui" }, types = true },
+            },
+        },
+    },
+    {
+        "Bilal2453/luvit-meta",
+        lazy = true,
+    }, -- optional `vim.uv` typings
+    {
+        'theHamsta/nvim-dap-virtual-text',
+    },
+    {
+        "jonboh/nvim-dap-rr",
+        dependencies = {"nvim-dap", "telescope.nvim"},
+    },
+}
