7df24dda45284088d5fc12be2793c8bf83292db6
Adrian Olschimke <a.olschimke@cronos.de>
2024-10-01 20:52:49 +0200

    added golang, and autoformat on save

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -40,3 +40,14 @@ require("lazy").setup({
   -- automatically check for plugin updates
   -- checker = { enabled = true },
 })
+
+-- Run gofmt + goimports on save
+--
+local format_sync_grp = vim.api.nvim_create_augroup("goimports", {})
+vim.api.nvim_create_autocmd("BufWritePre", {
+    pattern = "*.go",
+    callback = function()
+        require('go.format').goimports()
+    end,
+    group = format_sync_grp,
+})
