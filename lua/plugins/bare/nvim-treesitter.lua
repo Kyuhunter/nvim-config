d7aac106b155c93ed28c366160b145840a810f13
Adrian Olschimke <a.olschimke@cronos.de>
2025-11-03 09:05:48 +0100

    :wrench: enable TS auto install

--- lua/plugins/bare/nvim-treesitter.lua
+++ lua/plugins/bare/nvim-treesitter.lua
@@ -2,5 +2,9 @@ return {
     'nvim-treesitter/nvim-treesitter',
     lazy = false,
     branch = 'main',
-    build = ':TSUpdate'
+    build = ':TSUpdate',
+    opts = {
+        ensure_installed = { "angular", "typescript", "javascript", "java" },
+        auto_install = true,
+    }
 }
