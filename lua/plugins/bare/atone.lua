4e022b4218ca78a7bae701294b040d06e7a79f42
Adrian Olschimke <a.olschimke@cronos.de>
2025-11-20 16:34:12 +0100

    :sparkles: atone, doodle, smart-splits

---
+++ lua/plugins/bare/atone.lua
@@ -0,0 +1,8 @@
+return {
+    "XXiaoA/atone.nvim",
+    cmd = "Atone",
+    opts = {}, -- your configuration here
+    keys = {
+        { "<leader>u", "<cmd>Atone toggle<CR>", desc = "Toggle Atone-Tree" },
+    },
+}
--- lua/plugins/bare/telescope.lua
+++ lua/plugins/bare/telescope.lua
@@ -1,14 +1,60 @@
+local builtin = require("telescope.builtin")
+
 ---@module "telescope"
 return {
-    'nvim-telescope/telescope.nvim',
-    dependencies = {'nvim-lua/plenary.nvim'},
-    keys = function()
-        local builtin = require('telescope.builtin')
-        return {
-            {"<leader>pf", builtin.find_files, {desc = "Telescope find files"}},
-            {"<leader>pg", builtin.live_grep, {desc = "Telescope live grep"}},
-            {"<leader>pb", builtin.buffers, {desc = "Telescope list buffers"}},
-            {"<leader>pd", builtin.diagnostics, {desc = "Telescope list diagnostics"}},
-        }
-    end
+	"nvim-telescope/telescope.nvim",
+	dependencies = { "nvim-lua/plenary.nvim", "apdot/doodle" },
+	config = function()
+		local telescope = require("telescope")
+		telescope.setup({
+			extensions = {
+				doodle = {},
+			},
+		})
+
+		telescope.load_extension("doodle")
+	end,
+	keys = {
+		{
+			"<leader>pf",
+			builtin.find_files,
+			desc = "Telescope find files",
+		},
+		{
+			"<leader>pg",
+			builtin.live_grep,
+			desc = "Telescope live grep",
+		},
+		{
+			"<leader>pb",
+			builtin.buffers,
+			desc = "Telescope list buffers",
+		},
+		{
+			"<leader>pd",
+			builtin.diagnostics,
+			desc = "Telescope list diagnostics",
+		},
+		{
+			"<leader>dfn",
+			function()
+				require("telescope").extensions.doodle.find_notes()
+			end,
+			desc = "Doodle Find Notes",
+		},
+		{
+			"<leader>dff",
+			function()
+				require("telescope").extensions.doodle.find_files()
+			end,
+			desc = "Doodle Find Files",
+		},
+		{
+			"<leader>dft",
+			function()
+				require("telescope").extensions.doodle.find_templates()
+			end,
+			desc = "Doodle Find Templates",
+		},
+	},
 }
