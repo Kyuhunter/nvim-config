52ec31fd82a07434c07d96fc27e2b0922c081e14
Adrian Olschimke <a.olschimke@cronos.de>
2025-11-24 15:48:14 +0100

    :sparkles: Add doodle.nvim - keeping notes

--- lua/plugins/bare/doodle.lua
+++ lua/plugins/bare/doodle.lua
@@ -5,37 +5,49 @@ return {
 		"nvim-lua/plenary.nvim",
 		"nvim-telescope/telescope.nvim",
 	},
-	config = function()
-		require("doodle").setup({
-			settings = {
-				-- This is the only required setting for sync to work.
-				-- Set it to the absolute path of your private notes repository.
-				git_repo = os.getenv("HOME") .. "/notes",
-				sync = true,
-			},
-		})
-	end,
+	opts = {
+		settings = {
+			-- This is the only required setting for sync to work.
+			-- Set it to the absolute path of your private notes repository.
+			git_repo = os.getenv("HOME") .. "/notes",
+			sync = true,
+		},
+	},
 	keys = {
+        {
+            "<leader>dh",
+            function ()
+               require("doodle"):here()
+            end,
+            desc = "Doodle Here"
+        },
 		{
-			"<space>df",
+			"<leader>dv",
 			function()
 				require("doodle"):toggle_finder()
 			end,
 			desc = "Doodle Finder",
 		},
 		{
-			"<space>ds",
+			"<leader>ds",
 			function()
 				require("doodle"):sync()
 			end,
 			desc = "Doodle Sync",
 		},
 		{
-			"<space>dl",
+			"<leader>dl",
 			function()
 				require("doodle"):toggle_links()
 			end,
 			desc = "Doodle Links",
+		},
+		{
+			"<leader>dg",
+			function()
+				require("doodle"):graph_view()
+			end,
+			desc = "Doodle Graph View",
 		},
 	},
 }
