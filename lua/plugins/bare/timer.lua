66d9ef98b49e948eaefa0555452c9f14bcd12b27
Adrian Olschimke <a.olschimke@cronos.de>
2025-11-24 19:52:31 +0100

    :adhesive_bandage: which-key for timer

--- lua/plugins/bare/timer.lua
+++ lua/plugins/bare/timer.lua
@@ -1,22 +1,32 @@
 return {
-    "temelgunaydin/timer.nvim",
-    opts = {
-        options = { 5, 10, 25, 45, 55 }, -- customize durations
-    },
-    keys = {
-        {
-            "<leader>tf",
-            function()
-                require("timer").pick_and_start()
-            end,
-            desc = "Open Timer Duration Picker",
-        },
-        {
-            "<leader>tr",
-            function()
-                require("timer").showRemainingTime()
-            end,
-            desc = "Show Timer Remaining",
-        },
-    },
+	"temelgunaydin/timer.nvim",
+	opts = {
+		options = { 5, 10, 25, 45, 55 }, -- customize durations
+	},
+	config = function()
+		local whichkey = require("which-key")
+		whichkey.add({
+			{
+				"<leader>t",
+				group = "Timer",
+				icon = {
+					icon = "󰄉",
+				},
+			},
+			{
+				"<leader>tf",
+				function()
+					require("timer").pick_and_start()
+				end,
+				desc = "Open Timer Duration Picker",
+			},
+			{
+				"<leader>tr",
+				function()
+					require("timer").showRemainingTime()
+				end,
+				desc = "Show Timer Remaining",
+			},
+		})
+	end,
 }
