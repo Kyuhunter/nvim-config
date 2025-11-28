74bee94c468a09acc83ce7787766683167967c33
Adrian Olschimke <a.olschimke@cronos.de>
2025-11-28 12:01:35 +0100

    :wrench: Update treesitter version

--- lua/plugins/bare/nvim-treesitter.lua
+++ lua/plugins/bare/nvim-treesitter.lua
@@ -1,6 +1,13 @@
 return {
-    'nvim-treesitter/nvim-treesitter',
-    lazy = false,
-    branch = 'main',
-    build = ':TSUpdate',
+	"nvim-treesitter/nvim-treesitter",
+	lazy = false,
+	branch = "main",
+	version = "false",
+	build = ":TSUpdate",
+	opts = {
+		-- LazyVim config for treesitter
+		indent = { enable = true },
+		highlight = { enable = true },
+		folds = { enable = true },
+	},
 }
